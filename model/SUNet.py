4b171bc8ddcccdadcdb52ff5fa34f45a5f90c610
范植貿(Chi-Mao Fan) <qaz5517359@gmail.com>
2022-07-12 19:52:38 +0800

    Update SUNet.py

--- model/SUNet.py
+++ model/SUNet.py
@@ -28,3 +28,26 @@ class SUNet_model(nn.Module):
             x = x.repeat(1, 3, 1, 1)
         logits = self.swin_unet(x)
         return logits
+    
+if __name__ == '__main__':
+    from utils.model_utils import network_parameters
+    import torch
+    import yaml
+    from thop import profile
+    from utils.model_utils import network_parameters
+
+    ## Load yaml configuration file
+    with open('../training.yaml', 'r') as config:
+        opt = yaml.safe_load(config)
+    Train = opt['TRAINING']
+    OPT = opt['OPTIM']
+
+    height = 256
+    width = 256
+    x = torch.randn((1, 156, height, width))  # .cuda()
+    model = SUNet_model(opt)  # .cuda()
+    out = model(x)
+    flops, params = profile(model, (x,))
+    print(out.size())
+    print(flops)
+    print(params)
